a42e3a60524682fac1c6250ad469156f8ef690e6
12520026 <76806854+12520026@users.noreply.github.com>
2020-12-31 16:57:59 +0900

    test mail 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace JenkinsDemo.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(abcd)
+        public IActionResult Index(abcde)
         {
             return View();
         }
