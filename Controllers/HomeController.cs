a193a8a1d8194e7369e25a8eaac4cea00f70e84f
Phan Y Biển <phanybien@gmail.com>
2020-12-31 20:08:52 +0900

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace JenkinsDemo.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index(abcdefghighgh)
+        public IActionResult Index(abcdefghighghi)
         {
             return View();
         }
